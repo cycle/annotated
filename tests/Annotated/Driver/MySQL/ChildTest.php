c68569f0ef7bf8adcdc563f2056f29788b527449
Wolfy-J <wolfy.jd@gmail.com>
2019-03-28 18:51:30 +0300

    child classes location

---
+++ tests/Annotated/Driver/MySQL/ChildTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+ 
+namespace Cycle\Schema\Tests\Driver\MySQL; 
+
+class ChildTest extends \Cycle\Annotated\Tests\ChildTest 
+{ 
+    const DRIVER = "mysql"; 
+}
---
+++ tests/Annotated/Driver/SQLite/ChildTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+ 
+namespace Cycle\Schema\Tests\Driver\SQLite; 
+
+class ChildTest extends \Cycle\Annotated\Tests\ChildTest 
+{ 
+    const DRIVER = "sqlite"; 
+}
