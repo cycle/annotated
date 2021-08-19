faa6c78ebf48deb030abcd8b368a1fa605fc4162
Pavel Buchnev <butschster@gmail.com>
2021-08-19 14:34:49 +0300

    Replaced Spiral\Database with Cycle\Database (#22)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Spiral\Database;
+use Cycle\Database;
 
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', '1');
