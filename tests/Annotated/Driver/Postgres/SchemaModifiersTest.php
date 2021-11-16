14d78316a132cbfdbed5aafc6e7e2c438da89766
butschster <butschster@gmail.com>
2021-11-16 20:59:54 +0300

    Covers schema modifiers with tests

---
+++ tests/Annotated/Driver/Postgres/SchemaModifiersTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\Annotated\Tests\Driver\Postgres;
+
+class SchemaModifiersTest extends \Cycle\Annotated\Tests\SchemaModifiersTest
+{
+    const DRIVER = 'postgres';
+}
