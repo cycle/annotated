36ded894fdef1014cbc5ee402f22c61967b8cb22
Pavel Buchnev <butschster@gmail.com>
2021-11-17 10:47:25 +0300

    Covers schema modifiers with tests (#32)

---
+++ tests/Annotated/Driver/SQLite/SchemaModifiersTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\Annotated\Tests\Driver\SQLite;
+
+class SchemaModifiersTest extends \Cycle\Annotated\Tests\SchemaModifiersTest
+{
+    public const DRIVER = 'sqlite';
+}
