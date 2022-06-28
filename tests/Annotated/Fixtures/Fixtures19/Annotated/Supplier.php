c82adb25d2adc2dce127f1a2ce8bde4e6772e8f2
makedo <krivousa@gmail.com>
2022-06-28 03:56:46 +0300

    Add tests for parent class finder (#54)

---
+++ tests/Annotated/Fixtures/Fixtures19/Annotated/Supplier.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\Annotated\Tests\Fixtures\Fixtures19\Annotated;
+
+use Cycle\Annotated\Annotation\Entity;
+
+class Supplier extends Person
+{
+}
---
+++ tests/Annotated/Fixtures/Fixtures19/Attributed/LocalSupplier.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\Annotated\Tests\Fixtures\Fixtures19\Attributed;
+
+use Cycle\Annotated\Annotation\Entity;
+
+#[Entity]
+class LocalSupplier extends Supplier
+{
+}
