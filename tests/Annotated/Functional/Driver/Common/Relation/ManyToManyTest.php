5f5ad9d8bbda4b7e1904c020ae1e21ceb9ce2edf
Maxim Smakouz <m.smakouz@gmail.com>
2023-05-17 10:35:25 +0300

    Fix test

--- tests/Annotated/Functional/Driver/Common/Relation/ManyToManyTest.php
+++ tests/Annotated/Functional/Driver/Common/Relation/ManyToManyTest.php
@@ -73,7 +73,7 @@ abstract class ManyToManyTest extends BaseTest
         );
 
         // Check table
-        assertCount(3, $table->getIndexes());
+        assertCount(1, $table->getIndexes());
     }
 
     /**
